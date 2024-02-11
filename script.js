0d165e43723be510ca821ea2387da83502f595f1
ABDUL RAHIM <abdulrahim74264@zohomail.in>
2024-02-11 18:08:50 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// let a = [1,2,3,4,5,6,7]
+// console.log(a.reduce((a,b)=>{
+    // return a+b;
+// }))
+
+let a = 5
+let b = 8
+console.log(sum=((a,b)=> {
+     return a+b
+}))
+
+// console.log(arr)
+
+function sum(a,b){
+    return a+b
+}
+console.log(sum(a,b))
